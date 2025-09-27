bf7b56c329249ba376199cd1a5f421df2daaafd5
condoowner0607 <condoowner0607@gmail.com>
2025-09-27 13:09:16 -0400

    Added API config page

---
+++ pages/API_Configuration.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="API Configuration",
+    page_icon="🏢",
+    layout="wide"
+)
+
+st.title("🏢 API Configuration")
+st.write("Navigate to other pages using buttons or the sidebar.")
